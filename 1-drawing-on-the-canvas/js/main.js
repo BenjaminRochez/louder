f3a7bccc646c83bb15bd855f83e12c5bc97ff311
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-06-10 09:24:54 +0200

    Added waveform section

--- 1-drawing-on-the-canvas/js/main.js
+++ 1-drawing-on-the-canvas/js/main.js
@@ -1,59 +1,24 @@
-console.clear()
+console.clear();
 
-let posX = 0, posY = 0
-
-function preload() {
-}
+function preload() {}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
+  createCanvas(windowWidth, windowHeight);
 
-  rectMode(CENTER)
-  
-  // strokeWeight(5)
+  rectMode(CENTER);
+  strokeWeight(5);
 }
 
 function draw() {
-  background(0)
-  // fill()
-  // stroke()
-  
-  // Translate the drawing origin to the center.
-  translate(width / 2, height / 2)
-  
-  // Map the mouse coordinates to some values 
-  // that control the size of the rectangle.
-  const mapX = map(mouseX, 0, windowWidth, 0, 500)
-  const mapY = map(mouseY, 0, windowHeight, 0, 500)
+  background(51);
+  fill("red");
+  stroke("yellow");
+  translate(width / 2, height / 2);
+  rect(0, 0, 500, 500);
+  console.log("Hello");
 
-  // Draw a rectangle and control its position with the keyboard arrows
-  // and its size with the mouse.
-  rect(posX, posY, mapX, mapY)
-}
-
-function keyPressed () {
-  switch (keyCode) {
-    case LEFT_ARROW:
-      posX -= 10  
-      break;
-
-    case RIGHT_ARROW:
-      posX += 10  
-      break;      
-
-    case UP_ARROW:
-      posY -= 10  
-      break;
-
-    case DOWN_ARROW:
-      posY += 10  
-      break;           
-  
-    default:
-      break;
-  }
-}
-
-function windowResized() {
-	resizeCanvas(windowWidth, windowHeight)
+  push();
+  fill("green");
+  ellipse(0, 0, 100);
+  pop();
 }
